f20b437a3f00a08a97e7fe24e53855e8aa87215f
Lasse Koskela <lasse.koskela@gmail.com>
2009-01-17 16:44:32 +0200

    Added some tests

---
+++ test/test_git_log_entry.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestGitLogEntry < Test::Unit::TestCase
+  
+  COMMIT_HASH = "db4ace1c9ba6add9a2b08c153367e2b379f8fb4c"
+  COMMIT_DATE = Time.now
+  COMMIT_AUTHOR = "johndoe"
+  COMMIT_MESSAGE = "o hai"
+  
+  class FakeAuthor
+    def initialize(name); @name = name; end
+    def name; @name; end
+  end
+  
+  class FakeCommit
+    def sha; COMMIT_HASH; end
+    def date; COMMIT_DATE; end
+    def author; FakeAuthor.new(COMMIT_AUTHOR); end
+    def message; COMMIT_MESSAGE; end
+  end
+  
+  def test_initialization
+    entry = Blastr::GitLogEntry.new(FakeCommit.new)
+    assert_equal Blastr::GitRevision, entry.revision.class
+    assert_equal COMMIT_HASH, entry.revision.to_s
+    assert_equal COMMIT_DATE, entry.revision.date
+    assert_equal COMMIT_AUTHOR, entry.author
+    assert_equal COMMIT_MESSAGE, entry.comment
+  end
+end
---
+++ test/test_git_revision.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestGitRevision < Test::Unit::TestCase
+  
+  SAMPLE_SHA = "db4ace1c9ba6add9a2b08c153367e2b379f8fb4c"
+  
+  def setup
+    @revision_with_date = Blastr::GitRevision.new(SAMPLE_SHA, Time.now)
+    @revision_without_date = Blastr::GitRevision.new(SAMPLE_SHA)
+  end
+
+  def test_to_s
+    assert @revision_without_date.to_s == SAMPLE_SHA
+    assert @revision_with_date.to_s == SAMPLE_SHA
+  end
+
+  def test_before_comparison_between_dated_revisions
+    later_revision = Blastr::GitRevision.new(SAMPLE_SHA, @revision_with_date.date + 1)
+    assert @revision_with_date.before?(later_revision) == true
+    assert later_revision.before?(@revision_with_date) == false
+  end
+
+  def test_before_comparison_with_HEAD
+    head = Blastr::GitRevision.new("HEAD")
+    [@revision_with_date, @revision_without_date].each do |any_revision|
+      assert any_revision.before?(head) == true
+      assert head.before?(any_revision) == false
+    end
+  end
+end
