a196ebec218400ed068cbf40464ae1b8ca22c88d
Lasse Koskela <lasse.koskela@gmail.com>
2009-01-17 18:54:58 +0200

    Updated README and added an online help message for configuring people's names in ~/.blastr/people

--- lib/people/people.rb
+++ lib/people/people.rb
@@ -3,7 +3,20 @@ require 'yaml'
 module Blastr
   class People
     PEOPLE_FILE = File.expand_path("~/.blastr/people")
-    CONFIG_HELP_MESSAGE = ""
+    CONFIG_HELP_MESSAGE = <<EOS
+
+If you'd like Blastr to pronounce committers' real names instead of their
+UNIX names, you can create a YAML configuration file at
+
+    #{PEOPLE_FILE}
+
+The file format is like this:
+
+johndoe: John Doe
+janedoe: Jane Doe
+jimbo: James Ruby
+
+EOS
     
     puts CONFIG_HELP_MESSAGE unless File.file?(PEOPLE_FILE)
     
