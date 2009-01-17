31194d189ea9a2974d70dc1da9909f569aedb218
Lasse Koskela <lasse.koskela@gmail.com>
2009-01-17 12:48:07 +0200

    Added TTS stuff

---
+++ lib/blastr/tts.rb
@@ -0,0 +1,46 @@
+module Blastr
+
+  class TTSImplementation
+    attr_accessor :name
+    def initialize(name)
+      @name = name
+    end
+    def binary
+      %x[which #{@name}].strip
+    end
+    def available?
+      binary.empty? == false
+    end
+    def speak(msg)
+      %x[#{binary} "#{msg}"]
+    end
+  end
+
+  class Say < TTSImplementation
+    def initialize; super("say"); end
+  end
+
+  class Espeak < TTSImplementation
+    def initialize; super("espeak"); end
+  end
+
+  class Festival < TTSImplementation
+    def initialize; super("festival"); end
+    def speak(msg)
+      %x[echo "#{msg}" | #{binary} --tts]
+    end
+  end
+
+  def resolve_tts_system
+    return $tts unless $tts.nil?
+    [ Say.new, Espeak.new, Festival.new ].each do |impl|
+      return impl if impl.available?
+    end
+    raise "No TTS implementation found."
+  end
+
+  def speak(msg)
+    resolve_tts_system.speak(msg)
+  end
+
+end
