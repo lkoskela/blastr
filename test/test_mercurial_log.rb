07d2d07f9ccd90faa217193bee20695455b9bc9f
Lasse Koskela <lasse.koskela@gmail.com>
2009-02-21 13:21:26 +0200

    Refactoring. New test for special characters in commit message.

--- test/test_mercurial_log.rb
+++ test/test_mercurial_log.rb
@@ -7,17 +7,16 @@ class TestMercurialLogParsing < Test::Unit::TestCase
   end
 
   def test_one_entry
-    log = <<EOS
+    @log = <<EOS
 changeset 24:e8a2a4d187773a62f3309b0fa265c13425bc2258 by username:
 This is the commit message
 ----------------------------------------------------------------------
 EOS
-    entries = @hg.hg_log_entries(log)
-    assert_equal [ create_entry("24", "username", "This is the commit message") ], entries
+    should_produce_entries [ create_entry("24", "username", "This is the commit message") ]
   end
 
   def test_multiple_entries
-    log = <<EOS
+    @log = <<EOS
 changeset 10:e8a2a4d187773a62f3309b0fa265c13425bc2258 by alf:
 Revision 10
 ----------------------------------------------------------------------
@@ -28,14 +27,32 @@ changeset 8:4ae9f4bfdb98f65bd957e3fe72471b320150b38e by alf:
 Revision 8
 ----------------------------------------------------------------------
 EOS
-    entries = @hg.hg_log_entries(log)
-    assert_equal [ create_entry("10", "alf", "Revision 10"),
-                   create_entry("9", "bob", "Revision 9"),
-                   create_entry("8", "alf", "Revision 8") ], entries
+    should_produce_entries [
+      create_entry("10", "alf", "Revision 10"),
+      create_entry("9", "bob", "Revision 9"),
+      create_entry("8", "alf", "Revision 8") ]
   end
 
-  def test_one_multiline_entry_with_dashes_in_the_commit_message
-    log = <<EOS
+  def test_special_characters_in_commit_message
+    @log = <<EOS
+changeset 10:e8a2a4d187773a62f3309b0fa265c13425bc2258 by cat:
+Revision 10
+----------------------------------------------------------------------
+changeset 9:d7744e86dedc21a8ecf6bdb73eb191b8eaf5b0da by bob:
+Revision 9 with special ä and ö characters
+----------------------------------------------------------------------
+changeset 8:4ae9f4bfdb98f65bd957e3fe72471b320150b38e by alf:
+Revision 8
+----------------------------------------------------------------------
+EOS
+    should_produce_entries [
+      create_entry("10", "cat", "Revision 10"),
+      create_entry("9", "bob", "Revision 9 with special ä and ö characters"),
+      create_entry("8", "alf", "Revision 8") ]
+  end
+
+  def test_dashes_in_the_commit_message
+    @log = <<EOS
 changeset 123:e8a2a4d187773a62f3309b0fa265c13425bc2258 by ohair:
    ---
 -----
@@ -43,13 +60,11 @@ changeset 123:e8a2a4d187773a62f3309b0fa265c13425bc2258 by ohair:
 - Bullet
 ----------------------------------------------------------------------
 EOS
-    entries = @hg.hg_log_entries(log)
-    assert_equal [ create_entry("123", "ohair", "---\n-----\n-\n- Bullet") ], entries
+    should_produce_entries [ create_entry("123", "ohair", "---\n-----\n-\n- Bullet") ]
   end
 
-
   def test_multiple_multiline_entries_with_empty_lines_within_commit_message
-    log = <<EOS
+    @log = <<EOS
 changeset 2:e8a2a4d187773a62f3309b0fa265c13425bc2258 by cat:
 --
 ----------------------------------------------------------------------
@@ -60,13 +75,18 @@ changeset 0:4ae9f4bfdb98f65bd957e3fe72471b320150b38e by alf:
 
 ----------------------------------------------------------------------
 EOS
-        entries = @hg.hg_log_entries(log)
-        assert_equal [ create_entry("2", "cat", "--"),
-                       create_entry("1", "bob", "-"),
-                       create_entry("0", "alf", "") ], entries
-    end
+    should_produce_entries [
+      create_entry("2", "cat", "--"),
+      create_entry("1", "bob", "-"),
+      create_entry("0", "alf", "") ]
+  end
   
   private
+  
+  def should_produce_entries(expected_entries)
+    assert_equal expected_entries, @hg.hg_log_entries(@log)
+  end
+
   def create_entry(rev_string, author, comment)
     revision = Blastr::SourceControl::MercurialRevision.new(rev_string)
     Blastr::SourceControl::LogEntry.new(revision, author, comment)
--- test/test_svn_log.rb
+++ test/test_svn_log.rb
@@ -7,19 +7,20 @@ class TestSubversionLogParsing < Test::Unit::TestCase
   end
 
   def test_one_entry
-    log = <<EOS
+    @log = <<EOS
 ------------------------------------------------------------------------
 r123 | username | 2009-01-21 09:02:37 +0200 (Wed, 21 Jan 2009) | 1 line
 
 The commit message goes here
 ------------------------------------------------------------------------
 EOS
-    entries = @svn.svn_log_entries(log)
-    assert_equal [ Blastr::SourceControl::LogEntry.new(Blastr::SourceControl::SubversionRevision.new("123"), "username", "The commit message goes here") ], entries
+    should_produce_entries [
+      create_entry("123", "username", "The commit message goes here")
+    ]
   end
 
   def test_multiple_entries
-    log = <<EOS
+    @log = <<EOS
 ------------------------------------------------------------------------
 r2 | janedoe | 2009-01-22 09:02:00 +0200 (Wed, 21 Jan 2009) | 1 line
 
@@ -30,16 +31,14 @@ r1 | johndoe | 2009-01-21 09:03:00 +0200 (Wed, 21 Jan 2009) | 1 line
 First revision
 ------------------------------------------------------------------------
 EOS
-    actual_entries = @svn.svn_log_entries(log)
-    expected_entries = [
+    should_produce_entries [
       create_entry("2", "janedoe", "Second revision"),
       create_entry("1", "johndoe", "First revision")
-      ]
-    assert_equal expected_entries, actual_entries
+    ]
   end
 
   def test_one_multiline_entry
-    log = <<EOS
+    @log = <<EOS
 ------------------------------------------------------------------------
 r123 | username | 2009-01-21 09:02:37 +0200 (Wed, 21 Jan 2009) | 1 line
 
@@ -48,13 +47,13 @@ Second line
 Third line
 ------------------------------------------------------------------------
 EOS
-      entries = @svn.svn_log_entries(log)
-      assert_equal [ create_entry("123", "username", "First line\nSecond line\nThird line") ], entries
+    should_produce_entries [
+      create_entry("123", "username", "First line\nSecond line\nThird line")
+    ]
   end
 
-
   def test_multiple_multiline_entries
-    log = <<EOS
+    @log = <<EOS
 ------------------------------------------------------------------------
 r2 | bob | 2009-01-21 09:02:00 +0200 (Wed, 21 Jan 2009) | 1 line
 
@@ -67,12 +66,41 @@ First line of r1
 Second line of r1
 ------------------------------------------------------------------------
 EOS
-        entries = @svn.svn_log_entries(log)
-        assert_equal [ create_entry("2", "bob", "First line of r2\nSecond line of r2"),
-                       create_entry("1", "alf", "First line of r1\nSecond line of r1") ], entries
-    end
+    should_produce_entries [
+      create_entry("2", "bob", "First line of r2\nSecond line of r2"),
+      create_entry("1", "alf", "First line of r1\nSecond line of r1")
+    ]
+  end
+
+  def test_special_characters_in_commit_message
+    @log = <<EOS
+------------------------------------------------------------------------
+r3 | cat | 2009-01-21 09:03:00 +0200 (Wed, 21 Jan 2009) | 1 line
+
+Revision 3
+------------------------------------------------------------------------
+r2 | bob | 2009-01-21 09:02:00 +0200 (Wed, 21 Jan 2009) | 1 line
+
+Revision 2 with special ö character
+------------------------------------------------------------------------
+r1 | alf | 2009-01-21 09:01:00 +0200 (Wed, 21 Jan 2009) | 1 line
+
+Revision 1
+------------------------------------------------------------------------
+EOS
+    should_produce_entries [
+      create_entry("3", "cat", "Revision 3"),
+      create_entry("2", "bob", "Revision 2 with special ö character"),
+      create_entry("1", "alf", "Revision 1")
+    ]
+  end
   
   private
+  
+  def should_produce_entries(expected_entries)
+    assert_equal expected_entries, @svn.svn_log_entries(@log)
+  end
+  
   def create_entry(rev_string, author, comment)
     revision = Blastr::SourceControl::SubversionRevision.new(rev_string)
     Blastr::SourceControl::LogEntry.new(revision, author, comment)
