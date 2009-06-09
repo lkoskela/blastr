7642d7454780d374bf0fb5c6e2cc02fca0dfc730
Lasse Koskela <lasse.koskela@gmail.com>
2009-06-09 23:02:08 +0300

    Revert "Bumped version to 0.0.17"
    This reverts commit d6b9e9530c41ce8fc37dbb3230de74af74fcddd7.

--- lib/blastr.rb
+++ lib/blastr.rb
@@ -6,7 +6,7 @@ module Blastr
   require 'tts/tts.rb'
   require 'people/people.rb'
 
-  VERSION = '0.0.17'
+  VERSION = '0.0.16'
   COPYRIGHT = 'Copyright (c) 2009, Lasse Koskela. All Rights Reserved.'
   puts "Blastr #{VERSION}\n#{COPYRIGHT}\n"
   
