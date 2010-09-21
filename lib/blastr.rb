ab758cd73231f53489fc255a2c8bd6581d216d1e
Lasse Koskela <lasse.koskela@gmail.com>
2010-09-21 23:38:38 +0200

    Move printing of version and copyright into from lib/blastr.rb to bin/blastr.

--- lib/blastr.rb
+++ lib/blastr.rb
@@ -9,7 +9,6 @@ module Blastr
 
   VERSION = '0.0.18'
   COPYRIGHT = "Copyright (c) 2009-#{Time.now.year}, Lasse Koskela. All Rights Reserved."
-  puts "Blastr #{VERSION}\n#{COPYRIGHT}\n"
   
   def self.trap_and_exit(signal)
     trap(signal) {
