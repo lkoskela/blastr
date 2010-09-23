1f17e555ca92ab6db961ebfda1a552b504231659
Lasse Koskela <lasse.koskela@gmail.com>
2010-09-23 02:32:52 +0200

    Add tests for non-equality of LogEntry objects.

--- test/test_scm_logentry.rb
+++ test/test_scm_logentry.rb
@@ -3,11 +3,15 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestLogEntry < Test::Unit::TestCase  
   def test_equality
     assert_equal create_log_entry, create_log_entry
+    assert_not_equal create_log_entry(:revision => "1"), create_log_entry(:revision => "2")
+    assert_not_equal create_log_entry(:author => "John"), create_log_entry(:author => "Jane")
+    assert_not_equal create_log_entry(:comment => "Oh"), create_log_entry(:comment => "Yeah")
   end
   
   private
-  def create_log_entry
-    rev = Blastr::SourceControl::SubversionRevision.new("707")
-    Blastr::SourceControl::LogEntry.new(rev, "author", "comment")
+  def create_log_entry(args = {})
+    defaults = { :revision => "rev", :author => "author", :comment => "comment" }
+    args = defaults.update(args)
+    Blastr::SourceControl::LogEntry.new(args[:revision], args[:author], args[:comment])
   end
 end
