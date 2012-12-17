9f226350bc7370b34e12c8d298d8f480f7422bb1
Lasse Koskela <lasse.koskela@reaktor.fi>
2012-12-17 15:07:36 +0200

    Reformat code

--- lib/scm/scm.rb
+++ lib/scm/scm.rb
@@ -2,14 +2,17 @@ module Blastr::SourceControl
   
   class LogEntry
     attr_accessor :revision, :author, :comment
+
     def initialize(revision, author, comment)
       @revision = revision
       @author = author
       @comment = comment
     end
+
     def ==(other)
       @revision == other.revision and @author == other.author and @comment == other.comment
     end
+
     def to_s
       "revision #{@revision} by #{@author}: #{@comment}"
     end
